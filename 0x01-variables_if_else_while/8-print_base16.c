ea03aa7f453110de4bdd2d9d9a517dfdc47de437
Manu <manuela.riossosa@gmail.com>
2021-10-04 06:11:58 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - base 16
+  * Return: 0
+  */
+int main(void)
+{
+	int sd;
+
+	for (sd = '0' ; sd <= '9' ; sd++)
+	{
+		putchar(sd);
+	}
+	for (sd = 'a' ; sd <= 'f' ; sd++)
+	{
+		putchar(sd);
+	}
+		putchar('\n');
+
+	return (0);
+}
