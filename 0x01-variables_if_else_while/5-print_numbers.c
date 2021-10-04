265f1091acd82be6bdeb4a79e3019efce5808bab
Manu <manuela.riossosa@gmail.com>
2021-10-04 05:53:54 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - single digits
+  * Return: 0
+  */
+int main(void)
+{
+	int sd;
+
+	for (sd = '0' ; sd <= '9' ; sd++)
+	{
+		putchar(sd);
+	}
+		putchar('\n');
+
+	return (0);
+}
