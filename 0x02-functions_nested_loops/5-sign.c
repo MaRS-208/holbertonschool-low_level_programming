9f4b245b097783b2c97e4de0956398b11cb325b7
Manu <manuela.riossosa@gmail.com>
2021-10-06 18:49:51 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - prints number sign
+ * Return: sign
+ * @n: int variable
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('1');
+	}
+	else
+		if (n == 0)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0');
+		}
+		else
+		{
+			_putchar('-');
+			_putchar(',');
+			_putchar(' ');
+			return (-1);
+		}
+}
