380c79b5dd60c3cf097e593ceebfc731bbfe578e
Manu <manuela.riossosa@gmail.com>
2021-10-18 10:39:44 -0700

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory
+ * @dest: destinatary
+ * @src: source
+ * @n: num
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+		dest[c] = src[n];
+	return (dest);
+}
