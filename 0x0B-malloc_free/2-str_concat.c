3dd3ef8399ae750a1c9d2c3e037336992ab5ce6e
Manu <manuela.riossosa@gmail.com>
2021-10-27 18:53:54 -0700

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *out = NULL;
+
+	return (out);
+}
