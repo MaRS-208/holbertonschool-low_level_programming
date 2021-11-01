ab8e79c285f32e7e3b6b5eaffe5353db380eb694
Manu <manuela.riossosa@gmail.com>
2021-11-01 11:05:45 -0700

    0. PoppyII

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
 	char *name;
-float age;
+	float age;
 	char *owner;
 };
 #endif
