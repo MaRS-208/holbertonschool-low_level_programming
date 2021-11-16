83e01810162a7cf94807da70c0342c664b4dea7f
Manu <manuela.riossosa@gmail.com>
2021-11-16 05:03:47 -0800

    0. Print list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer
+ * Return: number of elements in a linked listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	return (0);
+}
