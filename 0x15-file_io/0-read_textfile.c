c8dc6a5af902ad775b190853332b6c22a8d9728c
Manu <manuela.riossosa@gmail.com>
2021-11-22 16:07:20 -0800

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (!filename)
-	{
 		return (0);
-	}
 	fd = open(filename, O_RDONLY | O_CREAT);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(1024);
+	buffer = malloc(letters);
 	if (!buffer)
 	{
 		close(fd);
