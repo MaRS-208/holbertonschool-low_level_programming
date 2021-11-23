80ff870ea63e3dbd5e410f677ed539872f6faaca
Manu <manuela.riossosa@gmail.com>
2021-11-22 16:00:18 -0800

    0. Tread lightly, she is near0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,6 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
