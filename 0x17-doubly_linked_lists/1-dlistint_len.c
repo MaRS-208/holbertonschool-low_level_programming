60a0758ff604596e3d78e8239b31ac5305c92990
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-05 07:34:56 -0700

    1. List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements of list
+ * @h:
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	if (h)
+	{
+		return (dlistint_len(h->next) + 1);
+	}
+	else
+		return (0);
+}
