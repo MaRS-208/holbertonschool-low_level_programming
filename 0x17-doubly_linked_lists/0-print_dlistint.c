d4e0d0e4d7b7776cc0a0bae0de6260a0535ed9b6
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-05 07:27:00 -0700

    0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of list
+ * @h:
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h)
+	{
+		printf("[%d]\n", h->n);
+		return (print_dlistint(h->next) + 1);
+	}
+	else
+		return (0);
+}
