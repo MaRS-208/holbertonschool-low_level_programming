22f7eb8e4bed6606c4dcb2bbf2770441cee1f057
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-16 15:06:51 -0700

    1. Insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Write a function that inserts a node as the left-child of another node
+ * @parent: root
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+}
