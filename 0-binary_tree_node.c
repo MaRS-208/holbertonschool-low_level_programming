262480ed997521eab5f006dd8f6e86b78b6ec91b
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-16 12:24:02 -0700

    0. New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Write a function that creates a binary tree node
+ * @parent: parent node
+ * @value: value of parent
+ * Return: node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
