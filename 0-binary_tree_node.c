3054251706dc80fdeaa4cc58ed820c0027eb01cc
MaRS-208 <manuela.riossosa@gmail.com>
2022-07-16 14:31:36 -0700

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,5 +8,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node;
 
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	node->n = value;
+	return (node);
 }
